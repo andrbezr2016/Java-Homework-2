e61cfd3a1b49ba2fd8288052ffc1db44a4410ca6
Bezrukov Andrey <andrbezr2016@yandex.ru>
2021-11-02 17:11:29 +0300

    some edits

--- src/com/andrbezr2016/taskshorstmann/chapter3/Main.java
+++ src/com/andrbezr2016/taskshorstmann/chapter3/Main.java
@@ -10,41 +10,9 @@ public class Main {
         Employee emp4 = new Employee(4, "Sofia", "G", 55000);
         Employee emp5 = new Employee(5, "Anna", "K", 35000);
         Employee[] employees = new Employee[]{emp1, emp2, emp3, emp4, emp5};
-        System.out.println("Average salary: " + average(employees));
+        System.out.println("Average salary: " + Measurable.average(employees));
         // Task 2
         System.out.println("==Task 2==");
-        System.out.println("The Employee with the largest salary: " + largest(employees));
-    }
-
-    /*
-    Provide a method double average(Measurable[] objects) that computes the average
-    measure. Use it to compute the average salary of an array of employees.
-     */
-    public static double average(Measurable[] objects) {
-        if (objects != null && objects.length != 0) {
-            double sum = 0;
-            for (Measurable obj : objects) {
-                sum += obj.getMeasure();
-            }
-            return sum / objects.length;
-        }
-        return 0;
-    }
-
-    /*
-    Continue with the preceding exercise and provide a method Measurable
-    largest(Measurable[] objects). Use it to find the name of the employee with
-    the largest salary. Why do you need a cast?
-     */
-    public static Measurable largest(Measurable[] objects) {
-        if (objects != null && objects.length != 0) {
-            Measurable result = objects[0];
-            for (Measurable obj : objects) {
-                if (obj.getMeasure() > result.getMeasure()) result = obj;
-            }
-            return result;
-        } else {
-            return null;
-        }
+        System.out.println("The Employee with the largest salary: " + Measurable.largest(employees));
     }
 }
--- src/com/andrbezr2016/taskshorstmann/chapter3/Measurable.java
+++ src/com/andrbezr2016/taskshorstmann/chapter3/Measurable.java
@@ -5,5 +5,48 @@ Provide an interface Measurable with a method double getMeasure() that measures
 an object in some way. Make Employee implement Measurable.
  */
 public interface Measurable {
+
     double getMeasure();
+
+    /*
+    Provide a method double average(Measurable[] objects) that computes the average
+    measure. Use it to compute the average salary of an array of employees.
+     */
+    static double average(Measurable[] objects) {
+        if (objects != null && objects.length != 0) {
+            double sum = 0;
+            for (Measurable obj : objects) {
+                sum += obj.getMeasure();
+            }
+            return sum / objects.length;
+        }
+        return 0;
+    }
+
+    /*
+    Continue with the preceding exercise and provide a method Measurable
+    largest(Measurable[] objects). Use it to find the name of the employee with
+    the largest salary.
+
+    Why do you need a cast?
+    Cast нужен для приведения типов.
+    Есть неявное безопасное upcast преобразование и явное небезопасное downcast преобразование.
+    Пример upcast:
+    Measurable measurable = new Employee(1, "Ivan", "C", 30000);
+    Но вызвать методы из Employee не сможем, только из Measurable.
+    Пример downcast:
+    Employee employee = (Employee) measurable;
+    Если здесь объект measurable не является Employee, то бросит исключение ClassCastException.
+     */
+    static Measurable largest(Measurable[] objects) {
+        if (objects != null && objects.length != 0) {
+            Measurable result = objects[0];
+            for (Measurable obj : objects) {
+                if (obj.getMeasure() > result.getMeasure()) result = obj;
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
 }
